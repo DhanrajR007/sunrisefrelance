e28a7ff75029654be7f1080e7788a1d09b1a7135
DhanrajR007 <dhanrajrathore1111@gmail.com>
2026-06-03 13:36:22 +0530

    Fix import paths for Banner and Hero components to match casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // import React from 'react'
 
-import Banner from "./components/banner"
+import Banner from "./components/Banner"
 import CampusLife from "./components/CampusLife"
 import Footer from "./components/Footer"
 import HeadMessage from "./components/Head"
-import Hero from "./components/hero"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import VisionSection from "./components/Vission"
 import WhySunrise from "./components/WhySunrise"
